43386b0ce4f4748df93945e1502dc09152d61246
Quinten Favorite <quintenfavorite97@gmail.com>
2022-03-11 16:15:28 -0500

    post request finalized

--- server.js
+++ server.js
@@ -1,94 +1,110 @@
 //express setup
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 //sqlite setup
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-app.use(express.static('public'));
+//body-parser
+const bodyParser = require("body-parser");
 
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get('/tickets', (req, res) => {
-    let db = new sqlite3.Database('tickets.db', (err) => {
-        if (err) return console.error(err.message);
-        console.log("sqlite3 initialized");
-    });
+app.use(express.static("public"));
 
-    const sqlGetAll = "SELECT * FROM Tickets";
-    db.all(sqlGetAll, [], (err, rows) => {
-        if (err) return console.error(err.message);
-        res.send(rows);
-    });
+app.get("/tickets", (req, res) => {
+  let db = new sqlite3.Database("tickets.db", (err) => {
+    if (err) return console.error(err.message);
+    console.log("sqlite3 initialized");
+  });
 
-    db.close();
+  const sqlGetAll = "SELECT * FROM Tickets ORDER BY date DESC";
+  db.all(sqlGetAll, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    res.send(rows);
+  });
+
+  db.close();
 });
 
-app.get('/tickets/wildcard', (req, res) => {
-    const db = new sqlite3.Database('tickets.db', (err) => {
-        if (err) return console.error(err.message);
-        console.log("sqlite3 initialized");
-    });
-    
-    const wildcard = req.query.wildcard;
-    console.log(wildcard);
+app.get("/tickets/wildcard", (req, res) => {
+  const db = new sqlite3.Database("tickets.db", (err) => {
+    if (err) return console.error(err.message);
+    console.log("sqlite3 initialized");
+  });
 
-    const sqlGetSearch = `SELECT * FROM Tickets WHERE "${wildcard}" IN (first_name, last_name, email, phone_number, employee, id) ORDER BY date DESC`;
-    console.log(sqlGetSearch);
-    db.all(sqlGetSearch, [], (err, rows) => {
-        if (err) return console.error(err.message);
-        res.send(rows);
-    });
+  const wildcard = req.query.wildcard;
+  console.log(wildcard);
 
-    db.close();
+  const sqlGetSearch = `SELECT * FROM Tickets WHERE "${wildcard}" IN (first_name, last_name, email, phone_number, employee, id) ORDER BY date DESC`;
+  console.log(sqlGetSearch);
+  db.all(sqlGetSearch, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    res.send(rows);
+  });
+
+  db.close();
 });
 
-app.post("/tickets", (req, res) => {
-    let db = new sqlite3.Database('tickets.db', (err) => {
-        if (err) return console.error(err.message);
-        console.log("sqlite3 initialized");
-    });
-    const input = [req.body.date, req.body.firstName, req.body.lastName, req.body.email, req.body.phoneNumber, req.body.brandModel, req.body.serial, req.body.issue, req.body.notes, req.body.employee];
-    const sqlCreateTicket = "INSERT INTO tickets (date, first_name, last_name, email, phone_number, brand_model, serial, issue, notes, employee) VALUES(?,?,?,?,?,?,?,?,?,?)";
-    db.run(sqlCreateTicket, input, (err) => {
-        if (err) return console.error(err.message);
-        res.json(input);
-        console.log('ticket created.')
-    });
+app.post("/tickets", urlencodedParser, (req, res) => {
+  let db = new sqlite3.Database("tickets.db", (err) => {
+    if (err) return console.error(err.message);
+    console.log("sqlite3 initialized");
+  });
+  const input = [
+    req.body.date,
+    req.body.firstName,
+    req.body.lastName,
+    req.body.email,
+    req.body.phoneNumber,
+    req.body.brandModel,
+    req.body.serial,
+    req.body.issue,
+    req.body.notes,
+    req.body.employee,
+  ];
+  const sqlCreateTicket =
+    "INSERT INTO tickets (date, first_name, last_name, email, phone_number, brand_model, serial, issue, notes, employee) VALUES(?,?,?,?,?,?,?,?,?,?)";
+  db.run(sqlCreateTicket, input, (err) => {
+    if (err) return console.error(err.message);
+    console.log("ticket created.");
+  });
 
-    db.close();
+  db.close();
 });
 
 app.patch("/tickets/:id", (req, res) => {
-    let db = new sqlite3.Database('tickets.db', (err) => {
-        if (err) return console.error(err.message);
-        console.log("sqlite3 initialized");
-    });
+  let db = new sqlite3.Database("tickets.db", (err) => {
+    if (err) return console.error(err.message);
+    console.log("sqlite3 initialized");
+  });
 
-    const sqlUpdateTicket = "UPDATE tickets SET first_name WHERE id = ?";
-    db.run(sqlUpdateTicket, ['zach', id], (err) => {
-        if (err) return console.error(err.message);
-        console.log('ticket updated');
-    });
-    
-    db.close();
+  const sqlUpdateTicket = "UPDATE tickets SET first_name WHERE id = ?";
+  db.run(sqlUpdateTicket, ["zach", id], (err) => {
+    if (err) return console.error(err.message);
+    console.log("ticket updated");
+  });
+
+  db.close();
 });
 
 app.delete("/tickets/:id", (req, res) => {
-    let db = new sqlite3.Database('tickets.db', (err) => {
-        if (err) return console.error(err.message);
-        console.log("sqlite3 initialized");
-    });
+  let db = new sqlite3.Database("tickets.db", (err) => {
+    if (err) return console.error(err.message);
+    console.log("sqlite3 initialized");
+  });
 
-    const sqlDelete = 'DELETE FROM tickets WHERE id = ?';
-    db.run(sqlDelete, req.params.id, (err) => {
-        if (err) return console.error(err.message);
-        console.log('ticket deleted');
-    });
+  const sqlDelete = "DELETE FROM tickets WHERE id = ?";
+  db.run(sqlDelete, req.params.id, (err) => {
+    if (err) return console.error(err.message);
+    console.log("ticket deleted");
+  });
 
-    db.close();
+  db.close();
 });
 
 app.listen(port, () => {
-    console.log(`server running and listening on port ${port}`);
+  console.log(`server running and listening on port ${port}`);
 });
